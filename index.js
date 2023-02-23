79b015b88cbcc5d47721fe06a68f46487c9195bc
John <kaebjohann@googlemail.com>
2023-02-23 11:33:51 +0100

    finish logic of the Rock Paper Scissor Game

--- index.js
+++ index.js
@@ -5,48 +5,78 @@ const selections=["rock","paper","scissor"]
 let userScore=0
 let computerScore=0
 //User & Computer choice
-let computerSelection=selections[getComputerChoice()]
+let computerSelection=""
 let userSelection=""
-console.log(userSelection)
 //a getComputerChoice function to pick one of the selections
 function getComputerChoice(choice){
-    return choice=Math.floor(Math.random()*3)
+    return choice=selections[Math.floor(Math.random()*3)]
 }
-//a playerSelection (should be case insensitive) should be a prompt at first
-function toLowerCase(string){
-    string=prompt("choose Rock, Paper or Scissor")
-    return string.toLowerCase()
+//a getUserChoice -> ask for choice -> valid choice -> return choice
+function getUserChoice(get){
+    get=userChoice(get)
+    validChoice(get)
+    return get
 }
-//check if checkValidChoice == true
-        //return choice
-        //later return playRound()
+//ask userChoice (should be case insensitive) should be a prompt at first
+function userChoice(string){
+    string=prompt("choose Rock, Paper or Scissor")   
+    return string=string.toLowerCase()
+}
+//checkValidChoice == true (check if user choice is in selections)
+        //return true
     //else
         //print "sorry i couldn't recognize your selection. please try again"
-        // re run playerSelection
-function checkValidChoice(userChoice){
+        // re run getUserChoice
+function validChoice(userChoice){
     if (selections.includes(userChoice)){
-        alert("Valid choice!")
+        alert("choice is Valid!")
+        return true 
     }else{
         alert("Choice is not Valid!")
-        playRound(userSelection,computerSelection)
+        getUserChoice(userSelection)  
     }
 }
+//I believe i need a getBothChoices function to get choices each round
 //playRound function which takes playerSelection + computerSelection as parameter
     //this function should return:
     //a winner with a string like "You Lose! Rock beats Scissor"
+    //or "It's a Draw!"
 function playRound(user,computer){
-    computer=computerSelection
-    user=toLowerCase(userSelection)
-    checkValidChoice(user)
-    // if user scissor & computer paper
-        //user wins
-        //user score++
-    //else if etc.
-    //if user == computer
-        //draw
+    user=getUserChoice(userSelection)
+    computer=getComputerChoice(computerSelection)
+    if (user==computer){
+        alert(`It's a Draw! You choose ${user} and the computer choose ${computer}!`)
+    }else if(user=="rock"&&computer=="paper"){
+        alert(`You Lost! you chose ${user} and the computer chose ${computer}`)
+        return computerScore+=1
+    }else if(user=="rock"&&computer=="scissor"){
+        alert(`You Won! you chose ${user} and the computer chose ${computer}`)
+        return userScore+=1
+    }else if(user=="paper"&&computer=="rock"){
+        alert(`You Won! you chose ${user} and the computer chose ${computer}`)
+        return userScore+=1
+    }else if(user=="paper"&&computer=="scissor"){
+        alert(`You Lost! you chose ${user} and the computer chose ${computer}`)
+        return computerScore+=1
+    }else if(user=="scissor"&&computer=="rock"){
+        alert(`You Lost! you chose ${user} and the computer chose ${computer}`)
+        return computerScore+=1
+    }else if(user=="scissor"&&computer=="paper"){
+        alert(`You Won! you chose ${user} and the computer chose ${computer}`)
+        return userScore+=1
+    }
     
 }
 //starGame function to playRound until user or computer reaches score of 5
 
-
-playRound()
+function startGame(){
+    while (userScore<=5 || computerScore<=5){
+        playRound()
+        if (userScore==5){
+            alert(`You Won! you reached ${userScore} Points while the computer reached ${computerScore} Points!`)
+            break
+        }else if(computerScore==5){
+            alert('You Won! you reached ${userScore} Points while the computer reached ${computerScore} Points!')
+        }
+    }
+}
