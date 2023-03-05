e75140f73b036d628e271f0d0f9caca88af8f66c
John <kaebjohann@googlemail.com>
2023-03-05 17:07:06 +0100

    add function to show and hide images depending on win case

--- index.js
+++ index.js
@@ -19,53 +19,58 @@ function getComputerChoice(choice){
     //or "It's a Draw!"
 function playRound(user){
     let computer=getComputerChoice(computerSelection)
-    forceRestart()
+    
     if (user==computer){
+        drawGif.style.visibility="visible"
         roundStatusEl.textContent=`It's a Draw! You choose ${user} and the computer choose ${computer}!`
     }else if(user=="rock"&&computer=="paper"){
         roundStatusEl.textContent=`You Lost! you chose ${user} and the computer chose ${computer}`
         computerScore+=1
         updateScoreEl()
         checkWinner()
+        showGif("paper","computer")
     }else if(user=="rock"&&computer=="scissor"){
         roundStatusEl.textContent=`You Won! you chose ${user} and the computer chose ${computer}`
         userScore+=1
         updateScoreEl()
         checkWinner()
+        showGif("rock","user")
     }else if(user=="paper"&&computer=="rock"){
         roundStatusEl.textContent=`You Won! you chose ${user} and the computer chose ${computer}`
         userScore+=1
         updateScoreEl()
         checkWinner()
+        showGif("paper","user")
     }else if(user=="paper"&&computer=="scissor"){
         roundStatusEl.textContent=`You Lost! you chose ${user} and the computer chose ${computer}`
         computerScore+=1
         updateScoreEl()
         checkWinner()
+        showGif("scissor","computer")
     }else if(user=="scissor"&&computer=="rock"){
         roundStatusEl.textContent=`You Lost! you chose ${user} and the computer chose ${computer}`
         computerScore+=1
         updateScoreEl()
         checkWinner()
+        showGif("rock","computer")
     }else if(user=="scissor"&&computer=="paper"){
         roundStatusEl.textContent=`You Won! you chose ${user} and the computer chose ${computer}`
         userScore+=1
         updateScoreEl()
         checkWinner()
+        showGif("scissor","user")
     }
 }
 
 function checkWinner(){
     if (userScore==5){
         let message = (`You Won! you reached ${userScore} Points while the computer reached ${computerScore} Points!`) 
-        roundStatusEl.textContent = "The game is over. Choose your Weapon or restart the game!"       
         lastGame(message)
-        alert(message)
+        gameOver()
     }else if(computerScore==5){
         let message = (`You Lost! you reached ${userScore} Points while the computer reached ${computerScore} Points!`)
-        roundStatusEl.textContent = "The game is over. Choose your Weapon or restart the game!"       
         lastGame(message)
-        alert(message)
+        gameOver()
     }   
 }
 
@@ -80,48 +85,97 @@ const rockBtn = document.querySelector("#rock-btn")
 const paperBtn = document.querySelector("#paper-btn")
 const scissorBtn = document.querySelector("#scissor-btn")
 const restartBtn = document.querySelector("#restart-btn")
+const gameOverDiv = document.querySelector("#gameOver")
 //player&computerScore text content = score
 playerScoreEl.textContent = userScore
 computerScoreEl.textContent = computerScore
 //when pressed rock,paper,scissor run playRound(selection)
 rockBtn.addEventListener("click", function(){
+    callBackGif()
     playRound(selections[0])
     btnAnimation(rockBtn)
+    
 })
 paperBtn.addEventListener("click", function(){
+    callBackGif()
     playRound(selections[1])
     btnAnimation(paperBtn)
+    
 })
 scissorBtn.addEventListener("click", function(){
+    callBackGif()
     playRound(selections[2])
     btnAnimation(scissorBtn)
+    
 })
-
+restartBtn.addEventListener("click", function(){
+    restartGame()
+} )
 //update scoreBoard after each round function
 function updateScoreEl(){
     playerScoreEl.textContent = userScore
     computerScoreEl.textContent = computerScore
 }
-//restartGame function
+//restartGame
 function restartGame(){
     userScore = 0
     computerScore = 0
     updateScoreEl()
+    gameOverDiv.style.visibility="hidden"
+    roundStatusEl.textContent = "Choose your Weapon"       
+
 }
 
-//force restart if someone already won. Will be unnecessary later
-function forceRestart(){
-    if (userScore == 5 || computerScore == 5){
-        restartGame()
-    }
-}
-
-// btnAnimation ()
+/* button click animation */
 function btnAnimation(x){
     x.classList.add("buttonActive")
     setTimeout(() => { x.classList.remove("buttonActive")
     }, 150);
 }   
-// add class .buttonActive to buttons 
-// for a time period of 0,7 sec
-// after 0,7 sec remove .buttonActive from buttons
+
+function lastGame(x){
+    lastGameEl.textContent=x
+}
+//gameOver Popup function
+function gameOver(){
+    gameOverDiv.style.visibility="visible"
+}
+
+//gif 
+
+const rockGif = document.querySelector("#dekuRockPunchGif")
+const scissorGif = document.querySelector("#zenitsuScissorCut")
+const paperGif = document.querySelector("#paperWinGif")
+const drawGif = document.querySelector("#drawGif")
+
+function reverseGif(x){
+    x.style.transform = "scaleX(-1)"; //this works
+}
+//function show rock gif if rock wins the round
+//check if user or computer won -> reverse if needed
+function showGif(whatGif,whatWinner){
+    if ( whatGif == "rock" && whatWinner == "computer"){
+        rockGif.style.visibility="visible"
+    }else if ( whatGif == "scissor" && whatWinner == "computer"){
+        scissorGif.style.visibility="visible"
+    }else if ( whatGif == "paper" && whatWinner == "computer"){
+        paperGif.style.visibility="visible"
+    }else if( whatGif == "rock" && whatWinner == "user"){
+        reverseGif(rockGif)
+        rockGif.style.visibility="visible"
+    }else if ( whatGif == "scissor" && whatWinner == "user"){
+        reverseGif(scissorGif)
+        scissorGif.style.visibility="visible"
+    }else if ( whatGif == "paper" && whatWinner == "user"){
+        reverseGif(paperGif)
+        paperGif.style.visibility="visible"
+    }
+}
+
+//hide gifs before each round again
+function callBackGif(){
+    rockGif.style.visibility="hidden"
+    scissorGif.style.visibility="hidden"
+    paperGif.style.visibility="hidden"
+    drawGif.style.visibility="hidden"
+}
